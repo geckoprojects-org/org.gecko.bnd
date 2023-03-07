6d8080a485f64d7b0bed4876ce1cb6a8ee4fcc42
Mark Hoffmann <m.hoffmann@data-in-motion.biz>
2023-03-07 09:59:40 +0100

    Update OSGi Test
    - update dependencies
    - update project license
    
    Signed-off-by: Mark Hoffmann <m.hoffmann@data-in-motion.biz>

--- org.gecko.bnd.osgitest.project.library/resources/projects/osgi-test/{{srcDir}}/{{basePackageDir}}/ExampleTest.java
+++ org.gecko.bnd.osgitest.project.library/resources/projects/osgi-test/{{srcDir}}/{{basePackageDir}}/ExampleTest.java
@@ -1,10 +1,12 @@
 /**
- * Copyright (c) 2012 - 2022 Data In Motion and others.
+ * Copyright (c) 2012 - 2023 Data In Motion and others.
  * All rights reserved. 
  * 
- * This program and the accompanying materials are made available under the terms of the 
- * Eclipse Public License v2.0 which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v20.html
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
  * 
  * Contributors:
  *     Data In Motion - initial API and implementation
